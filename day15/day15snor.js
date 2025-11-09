71da177845e38ea7133ba17047526f75aa57dc7b
haiphong <haiphong.hoang12@gmail.com>
2025-11-09 17:19:33 +0700

    day18.3

---
+++ day15/day15snor.js
@@ -0,0 +1,13 @@
+let i =0 ;
+if (i==0 );
+  i= i +1;
+  else;
+  i= i-1;
+while i >10
+    i =  i+1
+    else i <10
+    i =  i-1
+function checkvalue (i){
+ return (i+1);
+}
+ 
---
+++ day16/day16.js
@@ -0,0 +1,10 @@
+function rectangle(Rwidth,Rheight) {
+  return Rwidth * Rheight;
+}
+
+let value = rectangle (width= 6, height = 7);
+const Subject = (subject: "History"
+                 date: 6/7/2025
+                 teacher: Slarnox
+                 score: 0,1 point
+)
